f9058507857dc662f249542aea26f2c8de6ac1a7
aimee-liang <aimeeeliang@gmail.com>
2020-09-17 17:57:14 -0400

    First commit - one test not passing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +10,22 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path(@recipe)
   end
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredient_attributes: [:name, :quantity]
+    )
+  end
+
 end
